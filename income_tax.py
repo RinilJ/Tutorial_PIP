b2fbcaafe4241178286cf88ce901b700f7177642
Rinil Johns <160606540+RinilJ@users.noreply.github.com>
2025-01-19 20:51:31 +0530

    income_tax

--- income_tax.py
+++ income_tax.py
@@ -1,9 +1,9 @@
 Income=int(input("Enter your income:"))
-if income<250000:
+if Income<250000:
     print("No Income Tax")
-elif income<500000:
+elif Income<500000:
     print("Your tax:",Income*0.05)
-elif income<1000000:
+elif Income<1000000:
     print("Your tax:",Income*0.2)
 else:
     print("Your tax:",Income*0.3)
