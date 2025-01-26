16d9e9f698ea65d5cb66dc152fcafee34a114645
Rinil Johns <160606540+RinilJ@users.noreply.github.com>
2025-01-26 19:48:46 +0530

    income_tax

--- income_tax.py
+++ income_tax.py
@@ -1,26 +1,27 @@
-income=float(input('Enter your Annual Income: '))
-tax=0
-dedu=float(input('Enter deductions (if there\'s Any): '))
-ded_income=income-75000-dedu;
-if(ded_income<300000):
+annual_income = float(input('Enter your Annual Income: '))
+total_tax = 0
+deductions = float(input('Enter deductions (if there\'s any): '))
+taxable_income = annual_income - 75000 - deductions
+
+if taxable_income < 300000:
     print('No Tax to be paid')
-elif(ded_income<700000):
-    extra=float(ded_income-300000)
-    tax=extra*0.05
-    print("Tax to be paid = ", tax )
-elif(ded_income<1000000):
-    extra=float(ded_income-700000)
-    tax=20000+extra*0.10
-    print("Tax to be paid = ", tax )
-elif(ded_income<1200000):
-    extra=float(ded_income-1000000)
-    tax=50000+extra*0.15
-    print("Tax to be paid = ", tax )
-elif(ded_income<1500000):
-    extra=float(ded_income-1500000)
-    tax=80000+extra*0.20
-    print("Tax to be paid = ", tax )
-elif(ded_income>=1500000):
-    extra=float(ded_income-1500000)
-    tax=140000+extra*0.30
-    print("Tax to be paid = ", tax )
+elif taxable_income < 700000:
+    additional_income = taxable_income - 300000
+    total_tax = additional_income * 0.05
+    print("Tax to be paid =", total_tax)
+elif taxable_income < 1000000:
+    additional_income = taxable_income - 700000
+    total_tax = 20000 + additional_income * 0.10
+    print("Tax to be paid =", total_tax)
+elif taxable_income < 1200000:
+    additional_income = taxable_income - 1000000
+    total_tax = 50000 + additional_income * 0.15
+    print("Tax to be paid =", total_tax)
+elif taxable_income < 1500000:
+    additional_income = taxable_income - 1200000
+    total_tax = 80000 + additional_income * 0.20
+    print("Tax to be paid =", total_tax)
+elif taxable_income >= 1500000:
+    additional_income = taxable_income - 1500000
+    total_tax = 140000 + additional_income * 0.30
+    print("Tax to be paid =", total_tax)
